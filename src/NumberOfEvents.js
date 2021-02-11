6a67055e6982577422c1413e32ae244c37e45f49
willcrone11 <72156281+willcrone11@users.noreply.github.com>
2021-02-11 16:19:58 -0500

    Adds NumberOfEvents Component for choosing how many events to display at once

---
+++ src/NumberOfEvents.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class NumberOfEvents extends Component {
+
+  state = {
+    numberOfEvents: '24',
+  };
+
+  // When user changes number value
+  handleChange = (event) => {
+    const value = event.target.value;
+    this.props.updateEvents(null, value);
+    this.setState({ numberOfEvents: value });
+  };
+
+  render() {
+    
+    const { numberOfEvents } = this.state;
+    
+    return (
+      <div className="NumberOfEvents">
+        <label>Number of events</label>
+        <input
+          type='number'
+          className='number'
+          value={numberOfEvents}
+          onChange={this.handleChange}
+        />
+      </div>
+    );
+  }
+}
+
+export default NumberOfEvents;
