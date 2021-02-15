d02390b0a7395a53ac28e484f0bb9c444f94a5b1
willcrone11 <72156281+willcrone11@users.noreply.github.com>
2021-02-14 23:20:22 -0500

    Adds additional phrase to subheader

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ class App extends Component {
       <div className="App">
         <div id="header-container">
           <h1 id="header"><span id="devel">devel_</span><span id="up">Up</span><span id="plus">+</span></h1>
-          <p id="subheader">Explore upcoming events in <span id="webdev-header">Web Development</span> from around the world!</p>
+          <p id="subheader">Want to level-up your dev skills?<br/><br/>Explore upcoming events in <span id="webdev-header">Web Development</span> from around the world!</p>
         </div>
         <br/><br/>
         <label id="city-label">Select your city: </label>
