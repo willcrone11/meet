ae1c1b84b6174d7df45b91790fa5d5b7238db471
willcrone11 <72156281+willcrone11@users.noreply.github.com>
2021-02-26 14:54:22 -0500

    Adds atatus to project for testing purposes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -21,3 +22,4 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+atatus.config('74815b4dc5874d75a670130aff0bc5bb').install();
