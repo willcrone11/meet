fda701024c26307a6f61312bec2ca03494f3c89d
willcrone11 <72156281+willcrone11@users.noreply.github.com>
2021-03-08 19:33:02 -0500

    Adds missing semi-colon

--- src/api.js
+++ src/api.js
@@ -60,7 +60,7 @@ export const getEvents = async () => {
   if (!navigator.onLine) {
     const events = localStorage.getItem("lastEvents");
     NProgress.done();
-    return JSON.parse(events).events
+    return JSON.parse(events).events;
   }
 
   const token = await getAccessToken();
