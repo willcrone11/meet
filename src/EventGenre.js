a53df10f1ba15837d9e593bf6ca71fd09ef6687e
willcrone11 <72156281+willcrone11@users.noreply.github.com>
2021-03-17 16:32:34 -0400

    Fixes spacing and removes label before percentage on pie chart

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -7,33 +7,32 @@ const EventGenre = ({ events }) => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-      setData(() => getData());
+    setData(() => getData());
   }, [events]);
 
   const getData = () => {
-      const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'Angular'];
-      const data = genres.map((genre) => {
-          const value = events.filter(({ summary }) => summary.split(' ').includes(genre)).length;
-          return { name: genre, value };
-      })
+    const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'Angular'];
+    const data = genres.map((genre) => {
+      const value = events.filter(({ summary }) => summary.split(' ').includes(genre)).length;
+      return { name: genre, value };
+    })
   return data;
   };
 
   const colors = ['#ee5454', '#5f74f0', '#69eb13', '#13ebeb', '#eb13c7']
 
   return (
-      <ResponsiveContainer height={400}>
-          <PieChart>
-              <Pie data={data} cx="47%" cy="50%" labelLine={false} legendType="square" outerRadius={80} fill="#8884d8" datakey="value" label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}>
-                  {
-                      data.map((entry, index) => <Cell key={`cell-${index}`} fill={colors[index % colors.length]} name={entry.name}/>)
-                  }
-              </Pie>
-              <Legend layout="horizontal" align="center" verticalAlign="top">
-
-              </Legend>
-          </PieChart>
-      </ResponsiveContainer>
+    <ResponsiveContainer height={400}>
+      <PieChart>
+        <Pie data={data} cx="47%" cy="50%" labelLine={false} legendType="square" outerRadius={80} fill="#8884d8" datakey="value" label={({ percent }) => `${(percent * 100).toFixed(0)}%`}>
+          {
+            data.map((entry, index) => <Cell key={`cell-${index}`} fill={colors[index % colors.length]} name={entry.name}/>)
+          }
+        </Pie>
+        <Legend layout="horizontal" align="center" verticalAlign="top">
+        </Legend>
+      </PieChart>
+    </ResponsiveContainer>
   );
 }
 
