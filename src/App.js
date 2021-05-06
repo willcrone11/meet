d0134ea912400e30a29e93cbdbcc699925fb9323
willcrone11 <72156281+willcrone11@users.noreply.github.com>
2021-05-06 11:59:08 -0400

    Adds checkToken function

--- src/App.js
+++ src/App.js
@@ -74,7 +74,21 @@ class App extends Component {
   }
 
   async componentDidMount() {
+    const accessToken = localStorage.getItem("access_token");
+    const validToken = accessToken !== null ? await
+
+    checkToken(accessToken) : false;
+    this.setState({ tokenCheck: validToken });
+    if(validToken === true) this.updateEvents()
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = searchParams.get("code");
+    this.mounted = true;
+    if (code && this.mounted === true && validToken === false) {
+      this.setState({tokenCheck: true });
+      this.updateEvents()
+    }
     
+    this.mounted = true;
     getEvents().then((events) => {
       if (this.mounted) {
         this.setState({ events, locations: extractLocations(events) });
