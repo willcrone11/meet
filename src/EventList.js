c7f72384d636fef9f0f7269edbada4d8336ac8d0
willcrone11 <72156281+willcrone11@users.noreply.github.com>
2021-06-15 17:54:11 -0400

    fixes bug causing cities not to load

--- src/EventList.js
+++ src/EventList.js
@@ -1,23 +1,7 @@
 import React, { Component } from 'react';
 import Event from './Event';
-import { getEvents, extractLocations } from './api';
 
 class EventList extends Component {
-
-  state = {
-    events: [],
-    locations: [],
-  }
-
-  async componentDidMount() {
-    this.mounted = true;
-    getEvents().then((events) => {
-      if (this.mounted) {
-        this.setState({ events, locations: extractLocations(events) });
-      }
-    });
-    this.ifOnline();
-  }
 
   render() {
     
